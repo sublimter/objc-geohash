4c79c69c13587c5cc7869d51130f983b16199520
luoliangliang <luoliangliang@huami.com>
2018-07-20 09:27:47 +0800

    支持cartahge集成方式

---
+++ Projects/GeoHashExample/objc-geohash/objc_geohash.h
@@ -0,0 +1,18 @@
+//  objc_geohash.h
+//  Created on 2018/7/20
+//  Description <#文件描述#>
+
+//  Copyright © 2018年 Huami inc. All rights reserved.
+//  @author luoliangliang(luoliangliang@huami.com)
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for objc_geohash.
+FOUNDATION_EXPORT double objc_geohashVersionNumber;
+
+//! Project version string for objc_geohash.
+FOUNDATION_EXPORT const unsigned char objc_geohashVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <objc_geohash/PublicHeader.h>
+
+
